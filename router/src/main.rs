7e6dbd28bbd0576fc36c60c58a7954e4ec4efa34
Linus Basig <linus@basig.me>
2020-12-31 15:32:05 +0100

    init

---
+++ router/src/main.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate cerk_loader_file;
+
+use env_logger::Env;
+
+use cerk_config_loader_file::CONFIG_LOADER_FILE;
+use cerk_loader_file::{start, ComponentStartLinks};
+use cerk_router_broadcast::ROUTER_BROADCAST;
+use cerk_runtime_threading::THREADING_SCHEDULER;
+use cerk_port_dummies::PORT_PRINTER;
+use touch_port::PORT_TOUCH;
+
+fn main() {
+    env_logger::from_env(Env::default().default_filter_or("debug")).init();
+
+    start(ComponentStartLinks {
+        schedulers: fn_to_links![THREADING_SCHEDULER],
+        routers: fn_to_links![ROUTER_BROADCAST],
+        config_loaders: fn_to_links![CONFIG_LOADER_FILE],
+        ports: fn_to_links![PORT_TOUCH,PORT_PRINTER],
+    });
+}
---
+++ touch_port/src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+extern crate log;
+
+mod port;
+
+pub use self::port::{port_touch_start, PORT_TOUCH};
